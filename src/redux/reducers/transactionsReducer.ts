dd17d464e71f39738c219549fb196aca0a052373
Mateusz Olejarz <mateusz.d.olejarz@gmail.com>
2022-03-02 03:13:06 +0100

    Setup redux

---
+++ src/redux/reducers/transactionsReducer.ts
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Transaction } from '../../types';
+
+export interface TransactionState {
+    data: Array<Transaction>;
+}
+
+const initialState: TransactionState = {
+    data: [
+        {
+            id: '1321',
+            title: 'New book about Rust',
+            amountPln: 100,
+            amountEur: 22.82
+        },
+        {
+            id: '412',
+            title: 'Snacks for a football match',
+            amountPln: 20,
+            amountEur: 4.56
+        },
+        {
+            id: '21',
+            title: 'Bus ticket',
+            amountPln: 2.55,
+            amountEur: 0.58
+        }
+    ],
+};
+
+export const transactionSlice = createSlice({
+    name: 'transactions',
+    initialState,
+    reducers: {
+        setTransactions: (state, action) => {
+            state.data = action.payload;
+        },
+    }
+});
+
+export const { setTransactions } = transactionSlice.actions;
+
+export default transactionSlice.reducer;
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface Transaction {
+    id: string,
+    title: string,
+    amountPln: number,
+    amountEur: number,
+}
